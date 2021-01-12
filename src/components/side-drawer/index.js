1753b56be49624846c4391c619b7956bbb503f3c
Morgan Segura <morgansegura@gmail.com>
2021-01-11 22:37:55 -0800

    Adds menu items to side drawer

--- src/components/side-drawer/index.js
+++ src/components/side-drawer/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
 import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
 
 const SideDrawer = ({ anchor, state, onClick }) => {
 	return (
-		<Drawer anchor={anchor} open={state[anchor]} onClose={onClick}>
-			Some navigation here
+		<Drawer
+			anchor={anchor}
+			open={state[anchor]}
+			onClose={onClick}
+			style={{ backgroundColor: '#393939', width: '290' }}>
+			<List components='nav'>
+				<ListItem button onClick={() => console.log('Event Times')}>
+					Event Times
+				</ListItem>
+				<ListItem button onClick={() => console.log('Venue Info')}>
+					Venue Info
+				</ListItem>
+				<ListItem button onClick={() => console.log('Highlights')}>
+					Highlights
+				</ListItem>
+				<ListItem button onClick={() => console.log('Pricing')}>
+					Pricing
+				</ListItem>
+				<ListItem button onClick={() => console.log('Location')}>
+					Location
+				</ListItem>
+			</List>
 		</Drawer>
 	)
 }
